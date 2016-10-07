f19930db618b8b1c1592c70e78616ad197edc820
Robert Edmondson <robert@refactoru.com>
2016-10-07 10:17:12 -0600

    Front end routing part3

--- week3/front-end-routing/main.js
+++ week3/front-end-routing/main.js
@@ -1,22 +1,54 @@
 angular.module('CremeFraiche', ['ngRoute'])
-    .controller("SoFreshAndSoCremeCreme", cremeController);
-
+    .controller("SoFreshAndSoCremeCreme", cremeController)
+    .controller("CoffeeOrigins", originsController);
 
 angular.module('CremeFraiche')
-    .config(Router);
+    .config(myRouter);
 
-Router.$inject = ['$routeProvider'];
+myRouter.$inject = ['$routeProvider'];
 
-function Router($routeProvider) {
+function myRouter($routeProvider) {
 
-    $routeProvider.when('/', {
-        templateUrl: '/templates/home.html'
-    })
+    $routeProvider
+        .when('/', {
+            templateUrl: '/templates/home.html'
+        })
+        .when('/origins/:id', {
+            templateUrl: '/templates/origins.html',
+            controller: "CoffeeOrigins as oCtrl"
+        })
+        .when('/menu', {
+            templateUrl: '/templates/menu.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
 }   
 
 
 function cremeController() {
     var cCtrl = this;
 
-    cCtrl.greeting = "Welcome to our coffee shop!";
+    cCtrl.greeting = "Welcome to Bozo Coffee";
+
+    cCtrl.menu = [
+        {
+            name: "White chocolate mocha",
+            price: 7.50
+        },
+        {
+            name: "Black death",
+            price: 6.66
+        },
+        {
+            name: "Non-fat soy vanilla tofu latte with gluten free coffee and free trade, imitation, dolphin-safe whipped-style cream product. Just a sprinkling of GMOs and served in a all-natural 125% post-consumer recycled material hemp cup. Also comes with farm to table wifi",
+            price: 20
+        }
+    ]
+}    
+
+function originsController() {
+    var oCtrl = this;
+
+    oCtrl.greeting = "Origins";
 }    
