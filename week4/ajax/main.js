e719385fc75456c1878f7ed1736f06f2167dea84
shenanigan911 <steve@refactoru.com>
2016-10-11 15:37:30 -0600

    Mid-way update of ajax lecture

--- week4/ajax/main.js
+++ week4/ajax/main.js
@@ -3,7 +3,23 @@ angular.module('pokeapp',[])
 
 pokeController.$inject = ['$http'];
 
+// Using this form makes this order-dependent, so things that refer to pokeController must be
+// declared before and functions that it uses should be defined before
+// var pokeController = function($http) { ...}
+// Using this form, the function is not order dependent
 function pokeController($http) {
     var pCtrl = this;
     pCtrl.text = "Hello there";
+    
+    getPokemon($http);
+
+}   
+
+function getPokemon ($http) {
+    $http.get('http://pokeapi.co/api/v1/pokemon/139')
+    .then(function(res, status){
+        console.log("Success:",res);
+    }, function(res, status) {
+        console.log("Failure:",status);
+    })
 }
