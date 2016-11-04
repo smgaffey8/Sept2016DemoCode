ec44562042a8a0351d474e333fd34d950e5db3c8
Robert Edmondson <robert@refactoru.com>
2016-11-04 10:59:09 -0600

    Cookies and sessions lecture notes part 2

--- week7/cookies-and-sessions/server.js
+++ week7/cookies-and-sessions/server.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
     res.send("EAT COOKIES!!");
 });
 
+// Cookies
 app.get('/cookie', (req, res) => {
     // {cookie-key: cookie-value}
     res.cookie('cookie-key', 'cookie-value');
@@ -28,6 +29,34 @@ app.get('/tempCookie', (req, res)=>{
     res.send("Set temp cookie!");
 });
 
+// Sessions
+var sessions = {}; // this will be our sessions object
+// sessions are just arbitrary objects in server memory
+
+app.get('/session', (req, res)=>{
+    console.log("Cookies: ", req.cookies);
+
+    // 1st scenario: we don't know them and need to make a session for them
+    if(!req.cookies.login || !sessions[req.cookies.login]) {
+        var login = Math.random(); // generate a new session id
+        sessions[login] = { created: new Date() }; // store the created date on the user session object
+
+        // tell the browser to store the login cookie
+        res.cookie('login', login, {httpOnly: true});
+
+        res.send(`<h1>Welcome to the site, your account was created at ${sessions[login].created}</h1>`) // backticks for template literals
+    }
+    // 2nd scenario: we do know them and we already have a session
+    else {
+        res.send(`<h1>You've been a member since ${sessions[req.cookies.login].created}</h1>`);
+    }
+
+    // Our sessions object looks like this:
+    // {
+    //      0.8748756857648743726: {},
+    //      0.4647638476327463847: {}  
+    // }
+});
 
 app.listen(3030, (error) => {
     if(error){
